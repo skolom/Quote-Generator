1d75d706957221cde4d575f5e8fb89991d18d17c
skolom <nanaberimaoseiyaw@gmail.com>
2023-12-03 09:27:26 +0000

    Random Quote Generater

---
+++ index.js
@@ -0,0 +1,35 @@
+const btnEl = document.getElementById('btn');
+const quoteEl = document.getElementById("quote")
+const authEl = document.getElementById("author")
+
+
+async function getQuote() {
+
+    try {
+        btnEl.innerText = "Loading..."
+        btnEl.disable = true; 
+
+        const rsp = await axios.get((`http://api.quotable.io/random`)); 
+
+        quoteEl.innerText = rsp.data.content;
+    
+        authEl.innerText = ` ~ ${rsp.data.author}`;
+
+        btnEl.innerText = "Get A Quote"
+        btnEl.disable = false;
+
+
+
+
+    } catch (error) {
+
+        console.log(error);
+        quoteEl.innerHTML = `An error occured, try again later`;
+        authEl.innerText = `error happened`;
+    }
+
+}
+
+getQuote();
+
+btnEl.addEventListener("click", getQuote); 
